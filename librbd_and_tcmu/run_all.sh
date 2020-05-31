78b577d345d6208bd1ea546700f5b19fda859ccc
JiangYu <lnsyyj@hotmail.com>
2020-05-31 08:11:40 +0000

    update
    Signed-off-by: JiangYu <lnsyyj@hotmail.com>

--- librbd_and_tcmu/run_all.sh
+++ librbd_and_tcmu/run_all.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#./remote_bcache_dirty_data.sh
-#./run_ec_1m.sh  
-#./remote_bcache_dirty_data.sh
-#./run_ec_4k.sh  
-#./remote_bcache_dirty_data.sh
-#./run_ec_8k.sh  
+./remote_bcache_dirty_data.sh
+./run_ec_1m.sh  
+./remote_bcache_dirty_data.sh
+./run_ec_4k.sh  
+./remote_bcache_dirty_data.sh
+./run_ec_8k.sh  
 ./remote_bcache_dirty_data.sh
 ./run_re_1m.sh  
 ./remote_bcache_dirty_data.sh
