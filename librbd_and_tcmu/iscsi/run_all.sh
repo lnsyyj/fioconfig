5ce4adf3142838b1a302d1da6affb181460b58ee
JiangYu <lnsyyj@hotmail.com>
2020-06-04 14:09:23 +0000

    update
    Signed-off-by: JiangYu <lnsyyj@hotmail.com>

--- librbd_and_tcmu/iscsi/run_all.sh
+++ librbd_and_tcmu/iscsi/run_all.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-./remote_bcache_dirty_data.sh
+#./remote_bcache_dirty_data.sh
 ./run_ec_1m.sh  
-./remote_bcache_dirty_data.sh
+#./remote_bcache_dirty_data.sh
 ./run_ec_4k.sh  
-./remote_bcache_dirty_data.sh
+#./remote_bcache_dirty_data.sh
 ./run_ec_8k.sh  
 #./remote_bcache_dirty_data.sh
 #./run_re_1m.sh  
--- librbd_and_tcmu/iscsi/run_ec_8k.sh
+++ librbd_and_tcmu/iscsi/run_ec_8k.sh
@@ -19,6 +19,7 @@ source ${SHELL_FOLDER}/common_variable
 RW=(randwrite randread)
 for rw_name in ${RW[@]}
 do
+  ./remote_bcache_dirty_data.sh
   fio -ioengine=${IOENGINE} -numjobs=${NUMJOBS} -direct=${DIRECT} -size=${SIZE} -iodepth=${IODEPTH} -runtime=${RUNTIME} -rw=${rw_name} -ba=${BA} -bs=${BS} -filename=${FILE_NAME} -name="${IOENGINE}_${IODEPTH}_${RW}_${BS}_${POOL_TYPE}" > ${IOENGINE}_${IODEPTH}_${rw_name}_${BS}_${POOL_TYPE}.log
 done
 
